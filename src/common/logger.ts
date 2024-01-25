17c160cf3b0aa74399103c497cbedfbf31cf44e6
heliprokupetz14 <helipro47@gmail.com>
2024-01-25 13:29:51 +0200

    chore: change logger

--- src/common/logger.ts
+++
@@ -1,7 +0,0 @@
-import config from 'config';
-import jsLogger, { LoggerOptions } from '@map-colonies/js-logger';
-import { logMethod } from '@map-colonies/telemetry';
-
-const loggerConfig = config.get<LoggerOptions>('telemetry.logger');
-
-export default jsLogger({ ...loggerConfig, prettyPrint: loggerConfig.prettyPrint, hooks: { logMethod } });
--- src/providers/getProvider.ts
+++ src/providers/getProvider.ts
@@ -1,13 +1,14 @@
 import { container } from 'tsyringe';
+import { Logger } from '@map-colonies/js-logger';
 import { ProviderConfig, ProviderManager, ProvidersConfig } from '../common/interfaces';
 import { QueueFileHandler } from '../handlers/queueFileHandler';
 import { SERVICES } from '../common/constants';
-import logger from '../common/logger';
 import { NFSProvider } from './nfsProvider';
 import { S3Provider } from './s3Provider';
 
 function getProvider(config: ProviderConfig): S3Provider | NFSProvider {
   const queueFileHandler: QueueFileHandler = container.resolve(SERVICES.QUEUE_FILE_HANDLER);
+  const logger: Logger = container.resolve(SERVICES.LOGGER);
   if (config.type === 'S3') {
     return new S3Provider(config, logger, queueFileHandler);
   }
